6b18fc38f8be93422bf7c7a8c62b4e1273b52c8c
dragons96 <jinqunlong@black-unique.com>
2024-11-01 11:00:23 +0800

    feat: 支持scrapy命令

--- {{cookiecutter.project_name}}/src/extensions/gen/generators/scrapy.py
+++ {{cookiecutter.project_name}}/src/extensions/gen/generators/scrapy.py
@@ -269,8 +269,62 @@ FEED_EXPORT_ENCODING = "utf-8"
 # your spiders.
 ''', override=override)
 
+    def gen_scrapy_cmd():
+        cmd_dir = package_dir + os.sep + 'cmd'
+        scrapy_cmd_py = cmd_dir + os.sep + 'scrapy_cmd.py'
+        create_file(scrapy_cmd_py, '''import os
+import sys
+import click
+from loguru import logger
+from {{cookiecutter.package_name}}.config import cfg, get_config_dir
+from {{cookiecutter.package_name}}.logger import setup_loguru, setup_logging
+from {{cookiecutter.package_name}} import utils
+from seatools import ioc
+from typing import Optional
+from scrapy.cmdline import execute
+
+
+@click.command()
+@click.argument('args', nargs=-1)
+@click.option('--project_dir', default=None, help='项目目录, 未打包无需传该参数, 自动基于项目树检索')
+@click.option('--env', default='dev', help='运行环境, dev=测试环境, test=测试环境, pro=正式环境, 默认: dev')
+@click.option('--log_level', default='INFO',
+              help='日志级别, DEBUG=调试, INFO=信息, WARNING=警告, ERROR=错误, CRITICAL=严重, 默认: INFO')
+@click.version_option(version="1.0.0", help='查看命令版本')
+@click.help_option('-h', '--help', help='查看命令帮助')
+def main(project_dir: Optional[str] = None,
+         env: Optional[str] = 'dev',
+         log_level: Optional[str] = 'INFO',
+         args = None) -> None:
+    """Demo Scrapy cmd."""
+    # 如果是pyinstaller环境, 默认把当前路径设置为执行路径
+    if utils.is_pyinstaller_env():
+        os.environ['PROJECT_DIR'] = os.path.dirname(sys.executable)
+    if project_dir:
+        os.environ['PROJECT_DIR'] = project_dir
+    if env:
+        os.environ['ENV'] = env
+    # 运行ioc
+    ioc.run(scan_package_names='demo_scrapy',
+            config_dir=get_config_dir(),
+            exclude_modules=[],
+            )
+    # 设置日志文件
+    file_name = cfg().project_name + '.' + os.path.basename(__file__).split('.')[0]
+    setup_loguru('{}.log'.format(file_name), level=log_level, label='quotes')
+    setup_logging('{}.sqlalchemy.log'.format(file_name), 'sqlalchemy', level=log_level, label='quotes')
+    logger.info('运行成功, 当前项目: {}', cfg().project_name)
+    execute(['scrapy', *args])
+
+
+if __name__ == "__main__":
+    main()
+''')
+        add_poetry_script(project_dir, 'scrapy = "{{cookiecutter.package_name}}.cmd.scrapy_cmd:main"')
+
     gen_scrapy_cfg()
     gen_scrapy_dir()
+    gen_scrapy_cmd()
 
 
 def generate_scrapy_spider(project_dir: str, package_dir: str,
