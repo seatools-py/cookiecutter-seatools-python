416810148b2bc6e2dcd168866763c2b7614531fe
dragons96 <jinqunlong@black-unique.com>
2024-11-28 18:05:35 +0800

    update

--- {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/boot/ioc.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/boot/ioc.py
@@ -5,7 +5,6 @@ from {{ cookiecutter.package_name }}.config import get_config_dir
 def ioc_starter():
     # 运行ioc
     ioc.run(scan_package_names=[
-        'seatools.ioc.starters', # 自动添加遵循规范已安装的starter
         '{{cookiecutter.package_name}}'
     ],
             config_dir=get_config_dir(),
